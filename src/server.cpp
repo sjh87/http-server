522ec67725dc960f26513184e9bb69478e947af3
Stephen Horne <code@stephenhorne.com>
2024-06-20 12:09:08 -0400

    Make a response class to abstract creating an HTTP response payload

--- src/server.cpp
+++ src/server.cpp
@@ -7,9 +7,111 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <map>
 
 #define HTTP_200 "HTTP/1.1 200 OK\r\n"
 #define HTTP_404 "HTTP/1.1 404 Not Found\r\n"
+
+class HTTPHeaders {
+  std::map<std::string, std::string> headerMap;
+
+  public:
+
+    void add(std::string key, std::string value) {
+      headerMap[key] = value;
+    }
+
+    std::string toString() {
+      std::string headerStr;
+
+      for (auto [key, value]:headerMap) {
+        headerStr += key + ":" + value + "\n";
+      }
+
+      return headerStr;
+    }
+};
+
+class HTTPStatus {
+  std::string message;
+  std::string protocol = "HTTP/1.1";
+  unsigned value = 200;
+
+  public:
+    HTTPStatus(unsigned v)
+    {
+      switch (v)
+      {
+      case 404:
+        message = "Not Found";
+        value = 404;
+
+        break;
+
+      default:
+        message = "OK";
+        value = 200;
+
+        break;
+      }
+    }
+
+    HTTPStatus() {}
+
+    std::string toString() {
+      std::string statusString = "";
+
+      statusString += protocol + " " + std::to_string(value) + " " + message;
+
+      return statusString;
+    }
+};
+
+class HTTPBody {
+  std::string value;
+
+  public:
+
+    HTTPBody(std::string v) {
+      value = v;
+    }
+
+    HTTPBody() {
+      value = "";
+    }
+
+    std::string toString() {
+      return value;
+    }
+};
+
+class HTTPResponse {
+  HTTPStatus status;
+  HTTPHeaders headers;
+  HTTPBody body;
+
+  public:
+
+    HTTPResponse(HTTPStatus s) {
+      status = s;
+    }
+
+
+    HTTPResponse(HTTPStatus s, HTTPHeaders h) {
+      status = s;
+      headers = h;
+    }
+
+    HTTPResponse(HTTPStatus s, HTTPHeaders h, HTTPBody b) {
+      status = s;
+      headers = h;
+      body = b;
+    }
+
+    std::string toString() {
+      return status.toString() + "\r\n" + headers.toString() + "\r\n" + body.toString();
+    }
+};
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -24,7 +126,7 @@ int main(int argc, char **argv) {
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
-   return 1;
+   return EXIT_FAILURE;
   }
   
   // // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -32,7 +134,7 @@ int main(int argc, char **argv) {
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
-    return 1;
+    return EXIT_FAILURE;
   }
   
   struct sockaddr_in server_addr;
@@ -42,13 +144,13 @@ int main(int argc, char **argv) {
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 4221\n";
-    return 1;
+    return EXIT_FAILURE;
   }
   
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
-    return 1;
+    return EXIT_FAILURE;
   }
   
   struct sockaddr_in client_addr;
@@ -70,13 +172,13 @@ int main(int argc, char **argv) {
   std::string verb = url.substr(0, url.find_first_of(" "));
   std::string path = url.substr(url.find_first_of("/"), url.find_last_of(" ") - url.find_first_of("/"));
 
-  std::string status = path == "/" ? HTTP_200 : HTTP_404;
+  unsigned status = path == "/" ? 200 : 404;
 
-  std::string headers = "\r\n";
+  HTTPResponse response = HTTPResponse(status);
 
-  std::string response = status + headers;
+  std::string responseStr = response.toString();
 
-  send(socket_desc, response.data(), response.size() + 1, 0);
+  send(socket_desc, responseStr.data(), responseStr.size() + 1, 0);
 
   close(server_fd);
 
